380ee65c87c7dbfb399b561a5956bebae309bfa4
pyohei <mukaishohei76@gmail.com>
2015-09-10 23:28:58 +0900

    add automate release script

--- release.sh
+++ release.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # ----------------------------------------------------------------------
 # Release vims setting script.
 #
@@ -9,17 +8,37 @@
 # ----------------------------------------------------------------------
 
 VIM_BASE_DIR=${HOME}
-VIMRC=vimrc
-GVIMRC=gvimrc
-VIMRC_CNF=vimrc.cnf
-
+VIM_BACKUP_DIR=${HOME}/.vim-backup
 
 CUR_DIR=$(cd $(dirname $0);pwd)
 
-# Copy Vimrc 
+NOW=`date +"%Y%m%d%H%M%S"`
 
-# Copy Gvimrc 
 
-# Copy Vimrc configration
-# If vimrc.cnf doesn't exist on home dir, copy file.
-# If it has, doesn't copy.
+# --- Create backup directory ---
+if [ ! -d ${VIM_BACKUP_DIR} ]; then
+    mkdir -p ${VIM_BACKUP_DIR}
+fi
+
+
+backup() {
+    ORG_FILE=${VIM_BASE_DIR}/.$1
+    BACKUP_FILE=${VIM_BACKUP_DIR}/$1-${NOW}
+    if [ -e ${ORG_FILE} ]; then
+        cp ${ORG_FILE} ${BACKUP_FILE}
+    fi
+}
+
+backup vimrc
+backup gvimrc
+backup vimrc.cnf
+
+
+renew() {
+    CUR_FILE=${CUR_DIR}/$1
+    NEW_FILE=${VIM_BASE_DIR}/.$1
+    cp ${CUR_FILE} ${NEW_FILE} 
+}
+
+renew vimrc
+renew gvimrc
