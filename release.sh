3ab7fdd7abc48aa1278c78cd336040c960a5e3c4
pyohei <mukaishohei76@gmail.com>
2016-09-08 23:39:24 +0900

    Proceeded script

--- release.sh
+++ release.sh
@@ -14,16 +14,17 @@ CUR_DIR=$(cd $(dirname $0);pwd)
 
 NOW=`date +"%Y%m%d%H%M%S"`
 
-# TODO
-#   Delete cache file existing in .vim/var directory.
-#   Delete dein directory .vim/dein
-
 echo "Start --->>>"
-# --- Create backup directory ---
-# TODO
-#   Create `backup swap undo` directory in .vim/var
-#   Create dein directory
-#   Or create empty directory in this repository.
+
+# Delete cache
+deleteCache() {
+    # rm -rf $1
+    echo "--->>> Delete $1"
+}
+
+deleteCache ~/.vim/var
+deleteCache ~/.vim/dein
+
 if [ ! -d ${VIM_BACKUP_DIR} ]; then
     mkdir -p ${VIM_BACKUP_DIR}
 fi
@@ -33,9 +34,9 @@ fi
 backup() {
     ORG_FILE=${VIM_BASE_DIR}/.$1
     BACKUP_FILE=${VIM_BACKUP_DIR}/$1-${NOW}
-    if [ -e ${ORG_FILE} ]; then
-        cp -r ${ORG_FILE} ${BACKUP_FILE}
-    fi
+    # if [ -e ${ORG_FILE} ]; then
+        # cp -r ${ORG_FILE} ${BACKUP_FILE}
+    # fi
     echo ">>> backup .$1 to ${BACKUP_FILE} >>>"
 }
 
@@ -45,7 +46,7 @@ backup gvimrc
 renew() {
     CUR_FILE=${CUR_DIR}/$1
     NEW_FILE=${VIM_BASE_DIR}/.$1
-    cp -r ${CUR_FILE} ${NEW_FILE} 
+    # cp -r ${CUR_FILE} ${NEW_FILE} 
     echo ">>> Renew .$1 >>>"
 }
 
@@ -57,5 +58,7 @@ read -p 'Reset cnf file?[Y]: ' ANSWER
 if [ "$ANSWER" = "Y" ]; then
     renew vimrc.cnf
 fi
+
+# Delete .gitkeep file
 
 echo ">>>--- Finish"
